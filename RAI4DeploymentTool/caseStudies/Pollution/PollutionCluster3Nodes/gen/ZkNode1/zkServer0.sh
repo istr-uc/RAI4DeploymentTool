b530ac49aaa1ae9b9d0200734e226cc2d4f65403
Patricia <lopezpa@unican.es>
2023-04-21 10:26:49 +0200

    Restructuración de repositorio

---
+++ RAI4DeploymentTool/caseStudies/Pollution/PollutionCluster3Nodes/gen/ZkNode1/zkServer0.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/gestor/apache/Services/zookeeper/scripts
+/home/gestor/apache/Services/zookeeper/bin/zkService.sh start /home/gestor/apache/Services/zookeeper/config/zoo0.cfg
+sleep 30
---
+++ RAI4DeploymentTool/caseStudies/Pollution/PollutionMonoprocessor/gen/MainNode/zkServer2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/gestor/apache/Services/zookeeper/scripts
+/home/gestor/apache/Services/zookeeper/bin/zkService.sh start /home/gestor/apache/Services/zookeeper/config/zoo2.cfg
+sleep 30
